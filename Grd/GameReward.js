64c0b6cac312b4df04e5ad164bacfb9f2bbcd61e
gamereward <gamereward.io@gmail.com>
2018-06-01 16:57:55 +0700

    no message

--- Grd/GameReward.js
+++ Grd/GameReward.js
@@ -4,40 +4,82 @@ GrdUser=function(){
     this.address = '',
     this.balance = 0,
     this.token = '';
+    this.otp = false;
 };
 GameReward = function (options) {
-    const REWARD_TEST_URL = 'https://test.gamereward.com/appapi/';
-    const REWARD_MAIN_URL = 'https://gamereward.com/appapi/';
+    const REWARD_TEST_URL = 'https://test.gamereward.io/appapi/';
+    const REWARD_MAIN_URL = 'https://gamereward.io/appapi/';
     var opts = { 'net': 'TESTNET' };
     for (var attrname in options) { opts[attrname] = options[attrname]; }
     var serverUrl;
     var app_id = opts['appid'];
     var api_secret = opts['secret'];
-    var reward=this;
+    var reward = this;
+    this.User = null;
     if (opts.net == 'MAINNET') {
         serverUrl = REWARD_MAIN_URL;
     }
     else {
         serverUrl = REWARD_TEST_URL;
     }
+    restoreUser();
+    function setCookie(cname, cvalue, exsec) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exsec * 1000));
+        var expires = "expires=" + d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+    function getCookie(cname) {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+    function restoreUser() {
+        var st = getCookie('REWARD_ACCOUNT');
+        if (st && st.length > 0) {
+            reward.User = JSON.parse(st);
+        }
+        else {
+            reward.User = null;
+        }
+    }
+    window.onbeforeunload = saveUser;
+    function saveUser() {
+        var st=JSON.stringify(reward.User);
+        setCookie('REWARD_ACCOUNT', st, 20);
+    }
     function getRequestToken() {
         var t = Math.round(Math.round((new Date()).getTime() / 1000) / 15);
         var k = Math.floor(t % 20);
         var len = Math.floor(api_secret.length / 20);
         var str = api_secret.substr(k * len, len);
-        str = md5(str + t);
+        str = md5(str + t) + "&t=" + t;
         return str;
     }
     function createRequest(method, params,callback)
     {
         var xhr = new XMLHttpRequest();
-        var data = "api_id=" + app_id + "&api_secret=" + getRequestToken();
+        if ("withCredentials" in xhr) {
+        } else if (typeof XDomainRequest != "undefined") {
+            xhr = new XDomainRequest();
+        }
+        var data = "api_id=" + app_id + "&api_key=" + getRequestToken();
         if (params) {
             for (var vname in params) {
                 data = data + "&" + vname+"=" + params[vname];
             }
         }
-        if (reward.User != null) {
+        if (reward.User != null && reward.User.token!="") {
             data = data + "&token=" + reward.User.token;
         }
         xhr.open('POST', serverUrl + method, true);
@@ -55,20 +97,28 @@ GameReward = function (options) {
         };
         xhr.send(data);
     }
-    this.User = null;
-    this.login = function (username, password, callback) {
-        createRequest('login', { 'username': username, 'password':md5(password) }, function (result) {
-            reward.User = new GrdUser();
-            reward.User.username = result.username;
-            reward.User.email = result.email;
-            reward.User.address = result.address;
-            reward.User.balance = parseFloat(result.balance);
-            reward.User.token = result.token;
+    this.isLogged = function () {
+        return this.User != null;
+    }
+    this.login = function (username, password,otp, callback) {
+        createRequest('login', { 'username': username, 'password': md5(password), 'otp': otp }, function (result) {
+            if (result.error == 0) {
+                reward.User = new GrdUser();
+                reward.User.username = username;
+                reward.User.email = result.email;
+                reward.User.address = result.address;
+                reward.User.balance = parseFloat(result.balance);
+                reward.User.token = result.token;
+                if (result.optOptions) {
+                    reward.User.otp = result.optOptions > 0;
+                }
+            }
             callback(result);
         });
     };
     this.logout = function (callback) {
-        createRequest('logout',null, function (result) {
+        createRequest('logout', null, function (result) {
+            setCookie('REWARD_ACCOUNT', '');
             reward.User =null;
             callback(result);
         });
@@ -81,6 +131,32 @@ GameReward = function (options) {
     };
     this.requestResetPassword = function (usernameOrEmail,  callback) {
         createRequest('requestresetpassword', { 'username': usernameOrEmail }, function (result) {
+            callback(result);
+        });
+    };
+    this.qrcodeAddress = function (callback) {
+        createRequest('qrcode', { 'text': 'gamereward:' + this.User.address, 'type': '1' }, function (result) {
+            callback(result);
+        });
+    };
+    this.transfer = function (to,amount,otp, callback) {
+        createRequest('transfer', { 'to': username, 'value': amount, 'otp': otp }, function (result) {
+            if (result.error == 0) {
+                reward.user.balance -= amount;
+            }
+            callback(result);
+        });
+    };
+    this.getTransactionCount = function ( callback) {
+        createRequest('counttransactions', { }, function (result) {
+            if (result.error == 0) {
+                reward.user.balance -= amount;
+            }
+            callback(result);
+        });
+    };
+    this.getTransactions = function (start,count, callback) {
+        createRequest('transactions', { 'start': start, 'count': count }, function (result) {
             callback(result);
         });
     };
